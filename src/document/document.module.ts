ce3e685e794f29d45a37274e0664aebe802f360d
Isaay Sosa Hernández <93836824+iso11ml@users.noreply.github.com>
2026-05-01 16:53:17 -0600

    fix: add TypeOrmModule imports for Document and Signature modules

--- src/document/document.module.ts
+++ src/document/document.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { DocumentEntity } from './entities/document.entity';
 
 @Module({
   controllers: [DocumentController],
   providers: [DocumentService],
+    imports: [TypeOrmModule.forFeature([UserEntity, DocumentEntity])],
 })
 export class DocumentModule {}
--- src/signature/signature.module.ts
+++ src/signature/signature.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SignatureService } from './signature.service';
 import { SignatureController } from './signature.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SignatureEntity } from './entities/signature.entity';
+import { UserEntity } from 'src/user/entities/user.entity';
 
 @Module({
   controllers: [SignatureController],
   providers: [SignatureService],
+  imports: [TypeOrmModule.forFeature([SignatureEntity, UserEntity])],
 })
-export class SignatureModule {}
+export class SignatureModule { }
